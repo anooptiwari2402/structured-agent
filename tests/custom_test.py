b0aa55bd7f2b15f7b9ee28365c0e69df419b9651
Anoop Tiwari <anoop.ece.nitap@gmail.com>
2026-08-26 20:47:08 +0530

    Added test case

---
+++ tests/custom_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class Tests(unittest.TestCase):
+
+    def test_print_method(self):
+        print("Hi")
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+    # python -m unittest custom_test.py -v
