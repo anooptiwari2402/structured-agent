532de050fbdc665a82d1d5661bbfdda7ec507d1b
Anoop Tiwari <anoop.ece.nitap@gmail.com>
2026-08-26 19:28:30 +0530

    Added rich library

--- src/runner/runners.py
+++ src/runner/runners.py
@@ -1,13 +1,25 @@
+import logging
+
 from langchain_core.messages import HumanMessage
+
+from rich.markdown import Markdown
+from rich.console import Console
+
 from src.agent import create_agents
 
 
-async def run_agent(input: str):
+async def run_agent(query_input: str):
+
+    # logging.basicConfig(level=logging.INFO)
 
     agent = await create_agents()
 
-    response  = agent.invoke({
-        "messages":[HumanMessage(content=input)]
+    console = Console()
+
+    response  = await agent.ainvoke({
+        "messages":[HumanMessage(content=query_input)]
     })
 
-    print(response["messages"][-1].content)
+    content = response["messages"][-1].content
+
+    console.print(Markdown(content))
